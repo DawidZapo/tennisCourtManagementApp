a695bfcf4241cc8efe3f8b30db387eb3babfa160
DawidZapo <dawidzapo@gmail.com>
2023-12-07 10:49:18 +0100

    First commit

---
+++ src/main/java/com/springboot/tennisCourtManagementApp/TennisCourtManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.tennisCourtManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisCourtManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisCourtManagementAppApplication.class, args);
+	}
+
+}
