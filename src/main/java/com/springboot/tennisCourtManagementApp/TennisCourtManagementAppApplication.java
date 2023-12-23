ae0d6d46723fce81c57b5418a93cc671eee435b8
DawidZapo <dawidzapo@gmail.com>
2023-12-23 15:39:19 +0100

    reservation tile style added and js script changes

--- src/main/java/com/springboot/tennisCourtManagementApp/TennisCourtManagementAppApplication.java
+++ src/main/java/com/springboot/tennisCourtManagementApp/TennisCourtManagementAppApplication.java
@@ -14,12 +14,19 @@ public class TennisCourtManagementAppApplication {
 //	public CommandLineRunner commandLineRunner(CourtReservationService courtReservationService, CustomerService customerService) {
 //
 //		return runner -> {
-////			Customer customer = customerService.findCustomerByIdJoinFetch(2);
-////			customer.addReservation(
-////					new CourtReservation(2, LocalDate.of(2023,12,15), LocalTime.of(14,0), LocalTime.of(14,30),1,false,false)
-////			);
-////
-////			customerService.save(customer);
+//			Customer customer = customerService.findCustomerByIdJoinFetch(1);
+//			customer.addReservation(
+//					new CourtReservation(1, LocalDate.of(2023,12,23), LocalTime.of(14,0), LocalTime.of(16,0),1,false,false)
+//
+//			);
+//			customer.addReservation(
+//					new CourtReservation(2, LocalDate.of(2023,12,23), LocalTime.of(14,0), LocalTime.of(15,3),1,false,false)
+//
+//			);
+//
+//			customerService.save(customer);
+//			List<CourtReservation> reservations = courtReservationService.findAllByReservationDate(LocalDate.now());
+//			System.out.println(reservations.get(0));
 //
 //		};
 //	}
--- src/main/java/com/springboot/tennisCourtManagementApp/controller/MainController.java
+++ src/main/java/com/springboot/tennisCourtManagementApp/controller/MainController.java
@@ -1,13 +1,25 @@
 package com.springboot.tennisCourtManagementApp.controller;
 
+import com.springboot.tennisCourtManagementApp.entity.CourtReservation;
+import com.springboot.tennisCourtManagementApp.service.CourtReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Controller
 public class MainController {
+    private CourtReservationService courtReservationService;
+    @Autowired
+    public MainController(CourtReservationService courtReservationService) {
+        this.courtReservationService = courtReservationService;
+    }
+
     @GetMapping("/")
     public String home(Model model){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -15,6 +27,9 @@ public class MainController {
             String username = authentication.getName();
             model.addAttribute("username", username);
         }
+        List<CourtReservation> reservations = courtReservationService.findAllByReservationDate(LocalDate.now());
+        model.addAttribute("reservations", reservations);
+
         return "home";
     }
 }
