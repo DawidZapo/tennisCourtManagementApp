8b242ad6fc774d362ea9ca1478cdd5bf3553d85c
DawidZapo <dawidzapo@gmail.com>
2024-01-07 12:03:57 +0100

    Updated JavaScripts to display correct selected date with date-picker at / and at /daySummary. Date is read by URL

--- src/main/resources/static/js/updateSelectedDate.js
+++ src/main/resources/static/js/updateSelectedDate.js
@@ -1,6 +1,15 @@
 const datePicker = document.getElementById('date-picker');
 document.addEventListener('DOMContentLoaded', function() {
     const datePicker = document.getElementById('date-picker');
+    const urlParams = new URLSearchParams(window.location.search);
+
+    if (urlParams.has('date')) {
+        const urlDate = urlParams.get('date');
+        datePicker.value = urlDate;
+    } else {
+        const today = new Date().toISOString().slice(0, 10);
+        datePicker.value = today;
+    }
 
     datePicker.addEventListener('change', function() {
         const selectedDate = datePicker.value;
